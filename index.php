2896d614eb87558bb5acbec3ca33d124b56e57f3
Vania <Vania@192.168.1.47>
2020-06-05 16:05:48 +0200

    Test di connessione su tutti i db

--- index.php
+++ index.php
@@ -3,3 +3,11 @@
 require_once(__DIR__ . '/config/config_DBCORE.php');
 
 echo $connessione;
+
+require_once(__DIR__ . '/config/config_SPT.php');
+
+echo $connessione;
+
+require_once(__DIR__ . '/config/config_SSCP_data.php');
+
+echo $connessione;
