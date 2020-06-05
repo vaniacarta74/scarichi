46c66a8fc3c0197982b7ce3c2d5de7554717c421
Vania <Vania@192.168.1.47>
2020-06-05 19:24:33 +0200

    Inserimento parametro variabile nell'index

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+if (isset($_REQUEST['var'])) {
+    
+    $variabile = htmlspecialchars(strip_tags($_REQUEST['var']));
+    echo 'Variabile: ' . $variabile;
+} else {
+    echo 'Richiesta variabile';
+}
+
 require_once(__DIR__ . '/config/config_DBCORE.php');
 
 echo $connessione;
