c6bdba0af07a6fb184f0454a6abd655122af1940
Vania <Vania@192.168.1.47>
2020-06-05 20:21:04 +0200

    Dumping dei valori restituiti da dbcore

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if (isset($_REQUEST['var'])) {
     require_once(__DIR__ . '/config/config_DBCORE.php');
     //echo $conn;
     
-    $variabile = htmlspecialchars(strip_tags($_REQUEST['var']));
+    $var = htmlspecialchars(strip_tags($_REQUEST['var']));
     //echo 'Variabile: ' . $variabile;
     
     $query = 'SELECT
@@ -35,12 +35,32 @@ if (isset($_REQUEST['var'])) {
                     ON
                         scarichi.tipo_scarico = tipi_scarico.id_tipo_scarico
                 WHERE
-                    scarichi.variabile = ' . $variabile;
+                    scarichi.variabile = ' . $var;
     
     $stmt = sqlsrv_query($conn, $query);
     
     if ($stmt !== false) {
-        echo 'ok';
+        //echo 'ok';
+        
+        $i = 0;
+        while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+            $id_impianto = $row['impianto'];
+            $variabile_scarico = $row['variabile_scarico'];
+            $nome_scarico = $row['denominazione'];
+            $db[$i] = $row['db'];
+            $variabile[$i] = $row['variabile'];
+            $categoria[$i] = $row['categoria'];
+            $tipo_dato[$i] = $row['tipo_dato'];
+            $i++;
+        }
+
+        var_dump($id_impianto);
+        var_dump($variabile_scarico);
+        var_dump($nome_scarico);
+        var_dump($variabile);
+        var_dump($categoria);
+        var_dump($tipo_dato);
+        
     } else {
         die(print_r(sqlsrv_errors(), true));
     }
