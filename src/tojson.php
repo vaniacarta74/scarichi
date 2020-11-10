1f138abc4100053ecde9a189126f7b6266541a3b
Vania <Vania@192.168.1.47>
2020-11-10 20:26:56 +0100

    Ricaricamento tojson.php

--- src/tojson.php
+++ src/tojson.php
@@ -7,7 +7,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 try {
-    $request = checkRequest($_REQUEST);
+    $request = checkRequest($_REQUEST, true);
     
     $scarichi = loadScarichi($request);    
     $variabili_scarichi = loadVariabiliScarichi($scarichi);
@@ -22,10 +22,14 @@ try {
     
     $tabella = addTable($variabili, $scarichi, $dati_completi, $formule);
     
+    $dati_formattati = format($tabella, $request['field']);    
+    $dati_filtrati = filter($dati_formattati, $request['full'], 0);    
+    $dati_ripuliti = filter($dati_filtrati, false, NODATA);
+    
     http_response_code(200);
-    echo json_encode($tabella);    
+    echo json_encode($dati_ripuliti);   
 } catch (\Throwable $e) {
     http_response_code(400);
-    Error::errorHandler($e, DEBUG_LEVEL, true);
+    Error::errorHandler($e, DEBUG_LEVEL, 'json');
     exit();
 }
